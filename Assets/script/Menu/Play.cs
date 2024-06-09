c633646f8f4ffc45d7036c382387460831ed5909
FLXGloutfou <felix.hautin@hotmail.fr>
2024-06-09 18:06:23 +0200

    finito

--- Assets/script/Menu/Play.cs
+++ Assets/script/Menu/Play.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 public class Play : MonoBehaviour
 {
     public string mainPlayLoad;
-    public string protoypeLoad;
+    public string Load2;
+    public string Load3;
+    public string Load4;
     public GameObject PlayWindows;
     public void MainPlay()
     {
         SceneManager.LoadScene(mainPlayLoad);
     }
-    public void ProtoPlay()
+    public void PLay2()
     {
-        SceneManager.LoadScene(protoypeLoad);
+        SceneManager.LoadScene(Load2);
+    }
+    public void Play3()
+    {
+        SceneManager.LoadScene(Load3);
+    }
+    public void Play4()
+    {
+        SceneManager.LoadScene(Load4);
     }
     public void ClosePlayWindow()
     {
--- Assets/script/Menu/setting.cs
+++ Assets/script/Menu/setting.cs
@@ -1,9 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class setting : MonoBehaviour
 {
+    public string Menue;
     public AudioMixer audioMixer;
     public GameObject settingWindow;
 
@@ -21,5 +23,11 @@ public class setting : MonoBehaviour
     public void CloseSettingsWindow()
     {
         settingWindow.SetActive(false);
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(Menue);
     }
 }
